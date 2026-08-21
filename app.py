09bcee061056eb8b177dcdc96859e40acbd5be7b
sonarsandhya529 <sonarsandhya529@gmail.com>
2026-08-21 16:21:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-
+import plotly.express as px  
 # ------------------------------------------------------------------------------
 # ENTERPRISE CONFIGURATION PARAMETERS
 # ------------------------------------------------------------------------------
